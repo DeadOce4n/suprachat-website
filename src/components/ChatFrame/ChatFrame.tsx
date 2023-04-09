0e284d0385b88da7abc61ebbe3cd976390ca69da
ThyDevourer <pedro@ivanpartida.xyz>
2023-04-08 17:36:42 -0700

    fix: adjust chat modal height

--- src/components/ChatFrame/ChatFrame.tsx
+++ src/components/ChatFrame/ChatFrame.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const ChatFrame = ({ src, title }: Props) => (
   <iframe
-    className='h-full min-h-[80vh] w-full rounded-xl'
+    className='h-full min-h-[90vh] w-full rounded-xl'
     src={src}
     title={title}
     id='chat'
