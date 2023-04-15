be15191c1c8afbc18cf37fca6926f5131807bb84
ThyDevourer <pedro@ivanpartida.xyz>
2023-04-14 22:21:10 -0700

    chore: add credit to snippet author

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -13,9 +13,8 @@ export type Theme = {
   variant: ThemeVariant
 }
 
-// Borrowed from https://gist.github.com/steven-schmoll-at/5b4a886456c7cd9a854bb61f4a2a004a
+// --- Borrowed from https://gist.github.com/steven-schmoll-at/5b4a886456c7cd9a854bb61f4a2a004a
 type CombineAll<T> = T extends { [name in keyof T]: infer Type } ? Type : never
-
 type PropertyNameMap<T, IncludeIntermediate extends boolean> = {
   [name in keyof T]: T[name] extends object
     ?
@@ -23,14 +22,13 @@ type PropertyNameMap<T, IncludeIntermediate extends boolean> = {
         | (IncludeIntermediate extends true ? name : never)
     : name
 }
-
 type SubPathsOf<
   key extends keyof T,
   T,
   IncludeIntermediate extends boolean
 > = `${string & key}.${string & PathsOf<T[key], IncludeIntermediate>}`
-
 export type PathsOf<
   T,
   IncludeIntermediate extends boolean = false
 > = CombineAll<PropertyNameMap<T, IncludeIntermediate>>
+// ---
